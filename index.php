198a7f8b6129a8cb2b989912b23547119efaac6c
fiesken <vilmerzander@live.se>
2018-05-28 14:14:03 +0200

    recept
    recept?

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" style="text/css" href="menu.css">
+    <title></title>
+  </head>
+  <body>
+
+    <div class="top">
+      <div class="menu">
+
+        <a href="Test1.php">Test1</a>
+        <a href="Test1.php">Test2</a>
+        <a href="Test1.php">Test3</a>
+
+
+      </div>asdfg
+    </div>
+
+    <div class="middle">
+
+    </div>
+
+    <div class="footer">
+
+    </div>
+  </body>
+</html>
